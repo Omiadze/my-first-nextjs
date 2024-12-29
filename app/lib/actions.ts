414ae979b17910d0af25b030aeb05fd3ef69eedb
Omiadze <teona.omiadze.1@iliauni.edu.ge>
2024-12-29 21:13:19 +0400

    fix buttons.tsx error

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -132,13 +132,11 @@ export async function updateInvoice(
   redirect('/dashboard/invoices');
 }
 
-export async function deleteInvoice(id: string) {
+export async function deleteInvoice(id: string): Promise<void> {
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
-    return { message: 'Deleted Invoice.' };
   } catch (error) {
-    console.log(error)
-    return { message: 'Database Error: Failed to Delete Invoice.' };
+    console.error('Database Error: Failed to Delete Invoice.', error);
   }
 }
